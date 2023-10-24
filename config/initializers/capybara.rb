3a48809d6df8a477004909d60e635c0bde216080
Abdul Basit <abdul.basit@eritheialabs.io>
2023-10-24 20:13:08 +0500

    fixing binary issue

--- config/initializers/capybara.rb
+++ config/initializers/capybara.rb
@@ -3,10 +3,7 @@ require 'nokogiri'
 require 'capybara'
 # Configurations
 Capybara.register_driver :selenium do |app|
-  options = Selenium::WebDriver::Chrome::Options.new
-  options.binary = ENV['CHROME_BIN_PATH']
-  options.add_argument('--headless')
-  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
 end
 Capybara.javascript_driver = :chrome
 Capybara.configure do |config|
