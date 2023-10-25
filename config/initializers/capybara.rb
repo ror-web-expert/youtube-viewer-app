ae8f59df8bea4d0f08d519fdd547c146ea473e6b
Abdul Basit <abdul.basit@eritheialabs.io>
2023-10-25 11:15:19 +0500

    reverted changes

--- config/initializers/capybara.rb
+++ config/initializers/capybara.rb
@@ -2,19 +2,11 @@ require 'selenium-webdriver'
 require 'nokogiri'
 require 'capybara'
 # Configurations
-binary = ENV.fetch("GOOGLE_CHROME_BIN", nil)
-Capybara.register_driver(:headless_chrome) do |app|
-  options = {
-    args: %w[headless no-sandbox disable-dev-shm-usage disable-gpu remote-debugging-port=9222],
-    binary: binary
-  }.compact_blank
-  
-  capabilities = Selenium::WebDriver::Chrome::Options.new(options)
-  Capybara::Selenium::Driver.new(app, browser: :chrome, capabilities: capabilities)
+Capybara.register_driver :selenium do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
 end
 Capybara.javascript_driver = :chrome
 Capybara.configure do |config|
   config.default_max_wait_time = 10 # seconds
   config.default_driver = :selenium
 end
-
