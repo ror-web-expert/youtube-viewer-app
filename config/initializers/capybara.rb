9d2451ad8932c14cd673e2c2a1aba20b2d6cca51
Abdul Basit <abdul.basit@eritheialabs.io>
2023-10-31 14:49:12 +0500

    headless feature added

--- config/initializers/capybara.rb
+++ config/initializers/capybara.rb
@@ -6,7 +6,7 @@ Capybara.register_driver :selenium do |app|
   options = Selenium::WebDriver::Chrome::Options.new
 
   options.add_argument('--no-sandbox')
-  # options.add_argument('--headless')
+  options.add_argument('--headless')
   options.add_argument('--disable-gpu')
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
